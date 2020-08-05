3b1771aee8ce9a98d3dde7d57c6bc05594ff1d8c
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-08-05 09:29:03 +0800

    登陆系统

---
+++ 股票分析.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import baostock as bs
+
+#### 登陆系统 ####
+lg = bs.login()
+# 显示登陆返回信息
+print('login respond error_code:'+lg.error_code)
+print('login respond  error_msg:'+lg.error_msg)
