ad2df4b106254fceab9c62a8694ec351a01cc617
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-08-05 09:52:11 +0800

    结果集封装

--- 股票分析.py
+++ 股票分析.py
@@ -7,3 +7,16 @@ lg = bs.login()
 # 显示登陆返回信息
 print('login respond error_code:'+lg.error_code)
 print('login respond  error_msg:'+lg.error_msg)
+# 获取股票信息，返回集
+rs = bs.query_history_k_data("000001.SH", "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST",
+                             start_date='2010-01-01', end_date='2019-12-31', frequency="d", adjustflag="3")
+# print('query_history_k_data respond error_code:'+rs.error_code)
+# print('query_history_k_data respond  error_msg:'+rs.error_msg)
+# 股票结果集封装DataFrome对象
+dlist=[]
+while (rs.error_code=='0')and rs.next():
+    da=rs.get_row_data()
+    # print(da)
+    dlist.append(da)
+df=pd.DataFrame(dlist)
+print(df.head())
